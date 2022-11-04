5892d6c6dbc0e1c24720c8e6bab5816116bfb760
Matt Cooper <vtbassmatt@gmail.com>
2022-11-04 06:34:37 -0400

    commanders by color

--- stats_recos/urls.py
+++ stats_recos/urls.py
@@ -1,9 +1,18 @@
 from stats_recos import views
 from django.urls import path
+from .wubrg_utils import COLORS
 
 
-urlpatterns = [
-    path('top_cmdrs/', views.top_commanders, name="top-commanders"),
+urlpatterns = (
+[
+    path('cmdr/', views.commander_colors, name="cmdr"),
+] +
+[
+    path(f'cmdr/{name}/', views.commanders_by_color, filters, name=f'cmdr-{name}')
+    for name, _, filters in COLORS
+] +
+[
+    path('cmdr/top/', views.top_commanders, name="cmdr-top"),
     path('top_cards/', views.top_cards, name="top-cards"),
     path('partner_decks/', views.partner_decks, name="partner-decks"),
-]
+])
--- stats_recos/views.py
+++ stats_recos/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.db.models import Count, Q
-from decklist.models import Card, Deck
+from decklist.models import Card, Deck, Printing
+from .wubrg_utils import COLORS
 
 
 def top_commanders(request):
@@ -50,5 +51,51 @@ def partner_decks(request):
         "partner_decks.html",
         context={
             'decks': partner_decks[:20],
+        },
+    )
+
+
+def commander_colors(request):
+    return render(
+        request,
+        "commander_colors.html",
+        context={
+            'colors': [
+                (c[0], f'cmdr-{c[0]}') for c in COLORS
+            ],
+        },
+    )
+
+
+def commanders_by_color(request, w=False, u=False, b=False, r=False, g=False):
+    cmdrs = (
+        Card.objects
+        .prefetch_related('printings')
+        .filter(
+            identity_w=w,
+            identity_u=u,
+            identity_b=b,
+            identity_r=r,
+            identity_g=g,
+        )
+        .filter(
+            # TODO: banlist? silver cards which say "Summon"?
+            type_line__contains='Creature',
+            printings__rarity=Printing.Rarity.UNCOMMON,
+        )
+        .annotate(num_decks=Count(
+            'deck_list',
+            distinct=True,
+            filter=Q(deck_list__is_pdh_commander=True)
+        ))
+        .filter(num_decks__gt=0)
+        .order_by('-num_decks')
+    )
+
+    return render(
+        request,
+        "card_num_commands.html",
+        context={
+            'cards': cmdrs,
         },
     )
