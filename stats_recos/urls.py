a62df7ebe01416cb7ea899697efda7b72da01c7d
Matt Cooper <vtbassmatt@gmail.com>
2022-11-08 06:08:16 -0500

    strictly sort by card popularity

--- stats_recos/urls.py
+++ stats_recos/urls.py
@@ -24,6 +24,7 @@ urlpatterns = (
 ] +
 [
     path('cmdr/top/', views.top_commanders, name="cmdr-top"),
+    path('cmdr/<uuid:card_id>', views.single_cmdr, name="cmdr-single"),
     path('card/top/', views.top_cards, name="card-top"),
     path('card/<uuid:card_id>', views.single_card, name="card-single"),
     path('land/top/', views.top_lands, name="land-top"),
