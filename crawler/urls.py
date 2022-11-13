54c1248fa9a9e974089469a76482abd62475803d
Matt Cooper <vtbassmatt@gmail.com>
2022-11-13 06:51:20 -0500

    error clearing

--- crawler/urls.py
+++ crawler/urls.py
@@ -6,5 +6,6 @@ app_name = 'crawler'
 urlpatterns = [
     path('', views.crawler_index, name="index"),
     path('run/<int:run_id>/', views.run_detail, name="run-detail"),
+    path('run/<int:run_id>/clear', views.run_remove_error_hx, name="run-remove-error"),
     path('run/new/', views.new_run, name="run-new"),
 ]
--- crawler/views.py
+++ crawler/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
+from django.views.decorators.http import require_POST
+from django_htmx.http import HttpResponseClientRefresh
 from crawler.models import DeckCrawlResult, CrawlRun
 
 
@@ -34,3 +36,17 @@ def run_detail(request, run_id):
             'errored': run.state == CrawlRun.State.ERROR,
         },
     )
+
+
+@require_POST
+def run_remove_error_hx(request, run_id):
+    run = get_object_or_404(CrawlRun, pk=run_id)
+
+    if run.state == run.State.ERROR:
+        if run.next_fetch:
+            run.state = run.State.FETCHING_DECKS
+        else:
+            run.state = run.State.NOT_STARTED
+        run.save()
+    
+    return HttpResponseClientRefresh()
