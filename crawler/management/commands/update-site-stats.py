292724847d5ce59d62ffa7a5342b56004d8bd314
Matt Cooper <vtbassmatt@gmail.com>
2022-12-05 13:48:12 -0500

    bug fixes

--- crawler/management/commands/update-site-stats.py
+++ crawler/management/commands/update-site-stats.py
@@ -1,18 +1,10 @@
 from django.core.management.base import BaseCommand, CommandError
-from django.db.utils import DataError
-from django.utils.dateparse import parse_date
-import httpx
-import json_stream.httpx
 from decklist.models import Deck, SiteStat
 from ._mixins import LoggingMixin
 
 
-PROGRESS_EVERY_N_CARDS = 100
-
-class CantParseCardError(Exception): ...
-
 class Command(BaseCommand, LoggingMixin):
-    help = 'Ask Scryfall for card data'
+    help = 'Update site stats'
 
     def handle(self, *args, **options):
         legal_decks = (
