b73d2813c163b57a749fce83f8fe50bfd537efd1
Matt Cooper <vtbassmatt@gmail.com>
2023-01-15 12:17:05 -0500

    compute synergy scores in bulk

--- crawler/management/commands/compute-synergy.py
+++ crawler/management/commands/compute-synergy.py
@@ -1,7 +1,6 @@
 from ._command_base import LoggingBaseCommand
-from decklist.models import Card, Commander, SynergyScore
-from stats_recos.synergy import compute_synergy
-import math
+from decklist.models import Card, SynergyScore
+from stats_recos.synergy import compute_synergy_bulk
 
 
 class Command(LoggingBaseCommand):
@@ -10,52 +9,48 @@ class Command(LoggingBaseCommand):
     def handle(self, *args, **options):
         super().handle(*args, **options)
 
-        commanders = Commander.objects.all()
+        self._log('Computing card synergy scores')
 
-        for commander in commanders:
-            self._log(f"Working on {commander}")
-            
-            cards = (
-                Card.objects
-                .filter(deck_list__deck__commander=commander)
-                .distinct()
-                .exclude(id=commander.commander1.id)
-            )
-            if commander.commander2:
-                cards = cards.exclude(id=commander.commander2.id)
+        cards = (
+            Card.objects
+            .filter(deck_list__deck__pdh_legal=True)
+            .distinct()
+        )
 
-            # delete synergy scores for any cards which have
-            # dropped out of decks
-            deleted, _ = (
-                SynergyScore.objects
-                .filter(commander=commander)
-                .exclude(card__in=cards)
-                .delete()
-            )
-            if deleted > 0:
-                self._log(f"Deleted {deleted} irrelevant scores")
+        # delete synergy scores for any cards which have
+        # dropped out of decks
+        deleted, _ = (
+            SynergyScore.objects
+            .exclude(card__in=cards)
+            .delete()
+        )
+        if deleted > 0:
+            self._log(f"Deleted {deleted} irrelevant scores")
+
+        for card in cards:
+            self._log(f"Working on {card}")
 
             existing_scores = (
                 SynergyScore.objects
-                .filter(commander=commander)
+                .filter(card=card)
             )
 
             new_records = []
             update_records = []
 
-            for card in cards:
-                score = compute_synergy(commander, card)
+            scores = compute_synergy_bulk(card)
+            for commander, score in scores:
                 try:
-                    score_record = existing_scores.get(card=card)
+                    score_record = existing_scores.get(commander=commander)
                     update_records.append(score_record)
                 except SynergyScore.DoesNotExist:
                     score_record = SynergyScore(commander=commander, card=card)
                     new_records.append(score_record)
                 
                 score_record.score = score
-            
+
             SynergyScore.objects.bulk_create(new_records)
             SynergyScore.objects.bulk_update(update_records, ['score',])
             self._log(f"{len(new_records)} new scores, {len(update_records)} updated scores")
-        
+
         self._log("Done!")
--- stats_recos/synergy.py
+++ stats_recos/synergy.py
@@ -50,6 +50,71 @@ def compute_synergy(commander: Commander, card: Card):
     return round(percent_decks - percent_other_decks, 2)
 
 
+def compute_synergy_bulk(card: Card) -> list[tuple[Commander, float]]:
+    """For daily maintenance, it's faster to compute synergy for a card against
+    all commanders at once than going one by one."""
+    # calculate this card's appearance in all valid decks once
+    in_all_decks = (
+        commanders_of_identity(
+            card.identity_w,
+            card.identity_u,
+            card.identity_b,
+            card.identity_r,
+            card.identity_g,
+            allow_superset=True,
+        )
+        .exclude(
+            commander1=card,
+            commander2=card,
+        )
+        .aggregate(
+            appears=Count('decks', filter=Q(decks__pdh_legal=True) & Q(decks__card_list__card=card), distinct=True),
+            total=Count('decks', filter=Q(decks__pdh_legal=True), distinct=True),
+        )
+    )
+
+    if in_all_decks['appears'] == 0:
+        return []
+
+    appears = float(in_all_decks['appears'])
+    total = float(in_all_decks['total'])
+
+    # then calculate its appearance with each commander
+    with_each_commander = (
+        commanders_of_identity(
+            card.identity_w,
+            card.identity_u,
+            card.identity_b,
+            card.identity_r,
+            card.identity_g,
+            allow_superset=True,
+        )
+        .annotate(
+            appears=Count('decks', filter=Q(decks__pdh_legal=True) & Q(decks__card_list__card=card), distinct=True),
+            total=Count('decks', filter=Q(decks__pdh_legal=True), distinct=True),
+        )
+        .filter(appears__gt=0)
+    )
+
+    # For each commander, the card's synergy is
+    #   (appearance with commander) - (all appearances - appearances with commander)
+    #
+    # Note: this calculation will blow up if total - cmdr.total = 0.
+    # That is, if a card appears only with this commander and there are no
+    # other legal decks where it could appear. This would imply that there are
+    # no decks for any other same-color-identity commanders and no decks for
+    # any commander with a broader color identity, which is extremely unlikely.
+    # It isn't worth making this code less elegant to deal with a condition
+    # which will almost certainly never come up in practice.
+    return [
+        (cmdr, round(
+            (cmdr.appears / cmdr.total) - ((appears - cmdr.appears) / (total - cmdr.total)),
+            ndigits=2,
+        ))
+        for cmdr in with_each_commander
+    ]
+
+
 def commanders_of_identity(w, u, b, r, g, allow_superset=False):
     """Find all commanders with a color identity. If allow_superset
     is True, then find all commanders of at least that identity."""
