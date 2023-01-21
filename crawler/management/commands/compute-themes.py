5231b472ca3d2b8d68f6b60e62197202899f0e2c
Matt Cooper <vtbassmatt@gmail.com>
2023-01-21 16:38:59 -0500

    introduce data model for precomputing themes

---
+++ crawler/management/commands/compute-themes.py
@@ -0,0 +1,91 @@
+from ._command_base import LoggingBaseCommand
+from django.db.models import Count, Q, F, Value
+from django.core.management.base import CommandError
+from decklist.models import Deck, Commander, Theme, ThemeResult
+
+
+# HACK: see `Command._get_decks_for_theme` below for why this is necessary
+from django.db import connections
+USE_SQLITE_JSON_HACK = True if connections['default'].vendor == 'sqlite' else False
+
+
+class Command(LoggingBaseCommand):
+    help = 'Compute themes for commanders'
+
+    def handle(self, *args, **options):
+        super().handle(*args, **options)
+
+        self._log("Computing themes")
+
+        for theme in Theme.objects.all():
+            self._log(f"{theme}")
+            theme_decks = self._get_decks_for_theme(theme)
+
+            theme_cmdrs = (
+                Commander.objects
+                .annotate(
+                    theme_decks=Count(
+                        'decks',
+                        filter=Q(decks__in=theme_decks),
+                        unique=True,
+                    ),
+                    total_decks=Count('decks', unique=True),
+                )
+                .filter(theme_decks__gt=1)
+                .filter(theme_decks__gte=F('total_decks') * Value(theme.deck_threshold / 100.0))
+            )
+
+            # add or update commanders
+            for cmdr in theme_cmdrs:
+                ThemeResult.objects.update_or_create(
+                    theme=theme, commander=cmdr,
+                    defaults={
+                        'theme_deck_count': cmdr.theme_decks,
+                        'total_deck_count': cmdr.total_decks,
+                    }
+                )
+            
+            # delete irrelevant commanders
+            (
+                ThemeResult.objects
+                .filter(theme=theme)
+                .exclude(commander__in=[cmdr for cmdr in theme_cmdrs])
+                .delete()
+            )
+
+        self._log("Done!")
+
+    def _get_decks_for_theme(self, theme):
+        match theme.filter_type:
+            case Theme.Type.TRIBE:
+                return (
+                        Deck.objects
+                        .filter(pdh_legal=True)
+                        .annotate(theme_count=Count(
+                            'card_list',
+                            filter=Q(card_list__card__type_line__contains=theme.filter_text),
+                        ))
+                        .filter(theme_count__gt=theme.card_threshold)
+                    )
+
+            case Theme.Type.KEYWORD:
+                # HACK: `contains` is not supported on JSONField on SQLite.
+                # Because we're only storing a flat array, `regex` is a good
+                # enough approximation in this instance.
+                if USE_SQLITE_JSON_HACK:
+                    filter = Q(card_list__card__keywords__regex=rf'"{theme.filter_text}"')
+                else:
+                    filter = Q(card_list__card__keywords__contains=theme.filter_text)
+
+                return (
+                    Deck.objects
+                    .filter(pdh_legal=True)
+                    .annotate(keyword_count=Count(
+                        'card_list',
+                        filter=filter,
+                    ))
+                    .filter(keyword_count__gt=theme.card_threshold)
+                )
+
+            case _:
+                raise CommandError(f"Not prepared to handle a theme of type {theme.filter_type}")
--- decklist/models.py
+++ decklist/models.py
@@ -490,6 +490,33 @@ class Theme(models.Model):
         return f"{self.display_name} {self.get_filter_type_display()}"
 
 
+class ThemeResult(models.Model):
+    theme = models.ForeignKey(
+        Theme,
+        on_delete=models.CASCADE,
+        related_name='results',
+    )
+    commander = models.ForeignKey(
+        Commander,
+        on_delete=models.CASCADE,
+        related_name='theme_results',
+    )
+    theme_deck_count = models.IntegerField()
+    total_deck_count = models.IntegerField()
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['theme', 'commander'],
+                name='theme_commander',
+                violation_error_message='Commander + theme is not unique',
+            ),
+        ]
+
+    def __str__(self):
+        return f"{self.theme} for {self.commander}"
+
+
 class SynergyScore(models.Model):
     commander = models.ForeignKey(
         Commander,
